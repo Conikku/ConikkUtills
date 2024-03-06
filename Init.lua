320c59fe5d8cbc0b53a648a6876c3f5a0ce1d526
Conikku <72884174+Conikku@users.noreply.github.com>
2024-03-06 11:23:23 -0500

    Update Init.lua
    Fixed indenting

--- Init.lua
+++ Init.lua
@@ -1,6 +1,6 @@
 --[[
 	My (Conikku's) personal library I use for most of my games.
-  Open sourced on Roblox and GitHub
+  	Open sourced on Roblox and GitHub
   
 	Last Updated: 03/1/2024
 ]]
@@ -11,37 +11,37 @@ local IsStudio = RunService:IsStudio()
 local ConikkUtills = { }
 
 function ConikkUtills:TurnPositive(n : number)
-	  return math.abs(n)
+	return math.abs(n)
 end
 
 function ConikkUtills:NumbersMagnitude(N1 : number, N2 : number)
-	  return math.abs(N1 - N2)
+	return math.abs(N1 - N2)
 end
 
 function ConikkUtills:RoundToNearestInteger(n : number)
-	  return math.floor(n + 0.5)
+	return math.floor(n + 0.5)
 end
 
 function ConikkUtills:RoundToFarthestInteger(n : number)
-	  return math.ceil(n - 0.5)
+	return math.ceil(n - 0.5)
 end
 
 function ConikkUtills:StudioOnlyPrint(string : string)
-	  if IsStudio then
-		    print(string)
-	  end
+	if IsStudio then
+		print(string)
+	end
 end
 
 function ConikkUtills:StudioOnlyWarn(string : string)
-	  if IsStudio then
-		    warn(string)
-	  end
+	if IsStudio then
+		warn(string)
+	end
 end
 
 function ConikkUtills:StudioOnlyError(string : string, level : number?)
-	  if IsStudio then
-  		  error(string, level)
-	  end
+	if IsStudio then
+		error(string, level)
+	end
 end
 
 return ConikkUtills
