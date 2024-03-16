1f11dcd9a05b987c99f6227aab64f9c915685af0
Conikku <72884174+Conikku@users.noreply.github.com>
2024-03-16 11:57:55 -0400

    Update Init.lua
    Added new functions along with support for LightingProfileAPI and DeviceMaid. Also fixed bug with module breaking when calling it on server

--- Init.lua
+++ Init.lua
@@ -1,23 +1,56 @@
 --[[
 	My (Conikku's) personal library I use for most of my games.
   	Open sourced on Roblox and GitHub
-  
-	Last Updated: 03/11/2024
+	
+	For Documentation:
+  	https://github.com/Conikku/ConikkUtills
+	
+	Last Updated: 03/16/2024
 ]]
 
 local Players = game:GetService("Players")
 local CAS = game:GetService("ContextActionService")
 local RunService = game:GetService("RunService")
+local IsServer = RunService:IsServer()
 local IsStudio = RunService:IsStudio()
 
+-- My own Modules
+local AnyKey
+local Fade
+
 -- Third Party Open Source Modules
-local ContextActionUtility = require(script:WaitForChild("ContextActionUtility", 10))
+local LightingProfile = require(script:WaitForChild("LightingProfileAPI", 10))
+local ContextActionUtility
+local DeviceMaid
+
+if IsServer == false then
+	AnyKey = require(script:WaitForChild("AnyKey", 10))
+	Fade = require(script:WaitForChild("FadeScreen", 10))
+	
+	DeviceMaid = require(script:WaitForChild("DeviceMaid", 10))
+	ContextActionUtility = require(script:WaitForChild("ContextActionUtility", 10))	
+end
 
 -----------------
+
+-- Module Only Functions
+function ClientOnlyCheck()
+	if IsServer then warn("This function of ConikkUtills can only be called on the client") return end
+end
+
+function ServerOnlyCheck()
+	if IsServer == false then warn("This function of ConikkUtills can only be called on the server") return end
+end
+
+-----------------
+
 local ConikkUtills = { }
 ConikkUtills.ContextActionUtility = { }
 ConikkUtills.Round = { }
 ConikkUtills.StudioOnly = { }
+ConikkUtills.Input = { }
+ConikkUtills.LightingProfile = { }
+ConikkUtills.ScreenEffects = { }
 
 function ConikkUtills:GetPlayerThumbnail(player : Player, ThumbnailType : Enum.ThumbnailType, ThumbnailSize : Enum.ThumbnailSize)
 	local userId = player.UserId
@@ -62,56 +95,88 @@ function ConikkUtills.StudioOnly:Error(string : string, level : number?)
 	end
 end
 
+function ConikkUtills.ScreenEffects:Fade()
+	ClientOnlyCheck() return Fade
+end
+
+function ConikkUtills.Input:AnyKey()
+	ClientOnlyCheck() return AnyKey:Get()
+end
+
+function ConikkUtills.Input:GetPlatform()
+	ClientOnlyCheck() return DeviceMaid:getPlatform()
+end
+
+function ConikkUtills.Input:IsController()
+	ClientOnlyCheck() return DeviceMaid:isController()
+end
+
+function ConikkUtills.Input:IsVR()
+	ClientOnlyCheck() return DeviceMaid:isVR()
+end
+
+function ConikkUtills.Input:GetEnabled()
+	ClientOnlyCheck() return DeviceMaid:getEnabled()
+end
+
 function ConikkUtills.ContextActionUtility:BindAction(actionName : string, functionToBind : any , createTouchButton : boolean, ...)
-	ContextActionUtility.BindAction(actionName, functionToBind, createTouchButton, ...)
+	ClientOnlyCheck() ContextActionUtility.BindAction(actionName, functionToBind, createTouchButton, ...)
 end
 
 function ConikkUtills.ContextActionUtility:UnbindAction(actionName : string)
-	ContextActionUtility:UnbindAction(actionName)
+	ClientOnlyCheck() ContextActionUtility:UnbindAction(actionName)
 end
 
 function ConikkUtills.ContextActionUtility:BindActionAtPriority(actionName : string, functionToBind : any , createTouchButton : boolean, priorityLevel : any, ...)
-	ContextActionUtility:BindActionAtPriority(actionName, functionToBind, createTouchButton, priorityLevel, ...)
+	ClientOnlyCheck() ContextActionUtility:BindActionAtPriority(actionName, functionToBind, createTouchButton, priorityLevel, ...)
 end
 
 function ConikkUtills.ContextActionUtility:DisableAction(actionName : string, effectList)
-	ContextActionUtility:DisableAction(actionName, effectList)
+	ClientOnlyCheck() ContextActionUtility:DisableAction(actionName, effectList)
 end
 
 function ConikkUtills.ContextActionUtility:SetImage(actionName : string, image : string)
-	ContextActionUtility.SetImage(actionName, image)
+	ClientOnlyCheck() ContextActionUtility.SetImage(actionName, image)
 end
 
 function ConikkUtills.ContextActionUtility:SetTitle(actionName : string, title : string)
-	ContextActionUtility:SetTitle(actionName, title)
+	ClientOnlyCheck() ContextActionUtility:SetTitle(actionName, title)
 end
 
 function ConikkUtills.ContextActionUtility:GetButton(actionName : string)
-	ContextActionUtility:GetButton(actionName)
+	ClientOnlyCheck() ContextActionUtility:GetButton(actionName)
 end
 
 function ConikkUtills.ContextActionUtility:MakeButtonRound(actionName : string, amount : number)
-	ContextActionUtility:MakeButtonRound(actionName, amount)
+	ClientOnlyCheck() ContextActionUtility:MakeButtonRound(actionName, amount)
 end
 
 function ConikkUtills.ContextActionUtility:MakeButtonSquare(actionName : string)
-	ContextActionUtility:MakeButtonSquare(actionName)
+	ClientOnlyCheck() ContextActionUtility:MakeButtonSquare(actionName)
 end
 
 function ConikkUtills.ContextActionUtility:SetPressedColor(actionName : string, color : Color3)
-	ContextActionUtility:SetPressedColor(actionName, color)
+	ClientOnlyCheck() ContextActionUtility:SetPressedColor(actionName, color)
 end
 
 function ConikkUtills.ContextActionUtility:SetReleasedColor(actionName : string, color : Color3)
-	ContextActionUtility:SetReleasedColor(actionName, color)
+	ClientOnlyCheck() ContextActionUtility:SetReleasedColor(actionName, color)
 end
 
-function ConikkUtills.ContextActionUtility.LocalToolEquipped()
-	return ContextActionUtility.LocalToolEquipped
+function ConikkUtills.ContextActionUtility:LocalToolEquipped()
+	ClientOnlyCheck() return ContextActionUtility.LocalToolEquipped
 end
 
-function ConikkUtills.ContextActionUtility.LocalToolUnequipped()
-	return ContextActionUtility.LocalToolUnequipped
+function ConikkUtills.ContextActionUtility:LocalToolUnequipped()
+	ClientOnlyCheck() return ContextActionUtility.LocalToolUnequipped
+end
+
+function ConikkUtills.LightingProfile:ApplyProfile(Configuration : Configuration | LightingProfile.ProfileData)
+	LightingProfile:ApplyProfile(Configuration)
+end
+
+function ConikkUtills.LightingProfile:CreateProfileInstanceFromCurrentLighting()
+	return LightingProfile:CreateProfileInstanceFromCurrentLighting()
 end
 
 return ConikkUtills
